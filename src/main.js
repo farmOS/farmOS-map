efa2bd8b99228d01d5e7195de646b03bb2b5b9ea
Michael Stenta <mike@mstenta.net>
2019-07-10 23:51:10 -0400

    Wrap the map in a new window.farmOSMap() function that takes target as a parameter.

--- src/main.js
+++ src/main.js
@@ -2,17 +2,19 @@ import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 
-new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new XYZ({
-        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+window.farmOSMap = function(target) {
+  new Map({
+    target,
+    layers: [
+      new TileLayer({
+        source: new XYZ({
+          url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        }),
       }),
+    ],
+    view: new View({
+      center: [0, 0],
+      zoom: 2,
     }),
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2,
-  }),
-});
+  });
+};
