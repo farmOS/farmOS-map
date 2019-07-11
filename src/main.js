e3494b2f38b97a55f56660fa72f71df84d2fbf10
Michael Stenta <mike@mstenta.net>
2019-07-11 00:48:10 -0400

    Add a global window.farmOS.map object with a create() method and an array of map instances.

--- src/main.js
+++ src/main.js
@@ -2,19 +2,39 @@ import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 
-window.farmOSMap = function(target) {
-  new Map({
-    target,
-    layers: [
-      new TileLayer({
-        source: new XYZ({
-          url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+// Define window.farmOS if it isn't already.
+if (typeof window.farmOS === 'undefined') {
+  window.farmOS = {};
+}
+
+// Add a farmOS.map object that is available globaly.
+window.farmOS.map = {
+
+  // Array of farmOS map instances.
+  instances: [],
+
+  // Create a new farmOS map attached to a target element ID.
+  create(target) {
+    const map = new Map({
+      target,
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          }),
         }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
       }),
-    ],
-    view: new View({
-      center: [0, 0],
-      zoom: 2,
-    }),
-  });
+    });
+
+    // Add the map to the global instances array.
+    const instance = {
+      target,
+      map,
+    };
+    this.instances.push(instance);
+  },
 };
