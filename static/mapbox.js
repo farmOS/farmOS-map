5b78dd53c4eb63c2d4327caf15ac4951ce0fc61c
Michael Stenta <mike@mstenta.net>
2019-12-06 00:00:57 -0500

    Add MapBox Outdoors layer.

--- static/mapbox.js
+++ static/mapbox.js
@@ -6,10 +6,20 @@
       // Test MapBox API key (only works with farmos.github.io).
       var key = 'pk.eyJ1IjoiZmFybWllciIsImEiOiJjazB6ajZjODkwMWIzM2ptcDBvNjl4eGViIn0.oYDl6csdVuVzlB0hf2ju2Q';
 
-      // Add MapBox base layer.
+      // Add MapBox base layer: Satellite.
       var opts = {
         title: 'MapBox Satellite',
         url: 'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=' + key,
+        group: 'Base layers',
+        base: true,
+        visible: false,
+      };
+      instance.addLayer('xyz', opts);
+
+      // Add MapBox base layer: Outdoors.
+      var opts = {
+        title: 'MapBox Outdoors',
+        url: 'https://api.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=' + key,
         group: 'Base layers',
         base: true,
         visible: false,
