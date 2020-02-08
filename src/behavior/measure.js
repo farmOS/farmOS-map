e8912c3abdb36a38635bfb7987a6111a35a586ad
Michael Stenta <mike@mstenta.net>
2020-02-08 11:36:06 -0500

    Move formatArea() and formatLength() functions to src/utils/measure.js.

--- src/behavior/measure.js
+++ src/behavior/measure.js
@@ -2,7 +2,7 @@ import Overlay from 'ol/Overlay';
 import LineString from 'ol/geom/LineString';
 import Polygon from 'ol/geom/Polygon';
 import { unByKey } from 'ol/Observable';
-import { getArea, getLength } from 'ol/sphere';
+import { formatArea, formatLength } from '../utils/measure';
 
 // Store measurement overlays keyed by the ID of the feature they measure.
 const measures = {};
@@ -16,64 +16,8 @@ let map;
 let layer;
 let units;
 
-// Define conversion units and their coefficients.
-const m = 1;
-const km = 0.001;
-const ft = 3.28084;
-const mi = 0.0006213712;
-const sqM = 1;
-const ha = 0.0001;
-const sqFt = 10.76391;
-const ac = 0.0002471052;
-
 // Maintain a counter for feature IDs.
 let featureId = 0;
-
-/**
- * Format length output.
- * @param {LineString} line The line.
- * @return {string} The formatted length.
- */
-function formatLength(line) {
-  const length = getLength(line);
-  switch (units) {
-    case 'metric':
-      if (length * km > 0.25) {
-        return `${(length * km).toFixed(2)} km`;
-      }
-      return `${(length * m).toFixed(0)} m`;
-    case 'us':
-      if (length * mi > 0.25) {
-        return `${(length * mi).toFixed(2)} mi`;
-      }
-      return `${(length * ft).toFixed(0)} ft`;
-    default:
-      return '';
-  }
-}
-
-/**
- * Format area output.
- * @param {Polygon} polygon The polygon.
- * @return {string} Formatted area.
- */
-function formatArea(polygon) {
-  const area = getArea(polygon);
-  switch (units) {
-    case 'metric':
-      if (area * ha > 0.25) {
-        return `${(area * ha).toFixed(2)} ha`;
-      }
-      return `${(area * sqM).toFixed(0)} m<sup>2</sup>`;
-    case 'us':
-      if (area * ac > 0.25) {
-        return `${(area * ac).toFixed(2)} ac`;
-      }
-      return `${(area * sqFt).toFixed(0)} ft<sup>2</sup>`;
-    default:
-      return '';
-  }
-}
 
 /**
  * Get the feature ID. If no ID is set, generate one.
@@ -99,11 +43,11 @@ function updateMeasure(measure, geom) {
   let measurement;
   let coordinates;
   if (geom instanceof Polygon) {
-    measurement = formatArea(geom);
+    measurement = formatArea(geom, units);
     coordinates = geom.getInteriorPoint().getCoordinates();
   }
   if (geom instanceof LineString) {
-    measurement = formatLength(geom);
+    measurement = formatLength(geom, units);
     coordinates = geom.getLastCoordinate();
   }
   measure.setPosition(coordinates);
---
+++ src/utils/measure.js
@@ -0,0 +1,59 @@
+import { getArea, getLength } from 'ol/sphere';
+
+// Define conversion units and their coefficients.
+const m = 1;
+const km = 0.001;
+const ft = 3.28084;
+const mi = 0.0006213712;
+const sqM = 1;
+const ha = 0.0001;
+const sqFt = 10.76391;
+const ac = 0.0002471052;
+
+/**
+ * Format length output.
+ * @param {LineString} line The line.
+ * @param {string} units The system of measurement (metric or us).
+ * @return {string} The formatted length.
+ */
+export function formatLength(line, units = 'metric') {
+  const length = getLength(line);
+  switch (units) {
+    case 'metric':
+      if (length * km > 0.25) {
+        return `${(length * km).toFixed(2)} km`;
+      }
+      return `${(length * m).toFixed(0)} m`;
+    case 'us':
+      if (length * mi > 0.25) {
+        return `${(length * mi).toFixed(2)} mi`;
+      }
+      return `${(length * ft).toFixed(0)} ft`;
+    default:
+      return '';
+  }
+}
+
+/**
+ * Format area output.
+ * @param {Polygon} polygon The polygon.
+ * @param {string} units The system of measurement (metric or us).
+ * @return {string} Formatted area.
+ */
+export function formatArea(polygon, units = 'metric') {
+  const area = getArea(polygon);
+  switch (units) {
+    case 'metric':
+      if (area * ha > 0.25) {
+        return `${(area * ha).toFixed(2)} ha`;
+      }
+      return `${(area * sqM).toFixed(0)} m<sup>2</sup>`;
+    case 'us':
+      if (area * ac > 0.25) {
+        return `${(area * ac).toFixed(2)} ac`;
+      }
+      return `${(area * sqFt).toFixed(0)} ft<sup>2</sup>`;
+    default:
+      return '';
+  }
+}
