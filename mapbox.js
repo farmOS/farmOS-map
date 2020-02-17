198e72ab5edb3078d49ea8d81302e25e4ccf883f
Michael Stenta <mike@mstenta.net>
2020-02-17 13:21:25 -0500

    Publish v0.9.4+1 to GitHub Pages.

--- mapbox.js
+++ mapbox.js
@@ -6,16 +6,6 @@
       // Test MapBox API key (only works with farmos.github.io).
       var key = 'pk.eyJ1IjoiZmFybWllciIsImEiOiJjazB6ajZjODkwMWIzM2ptcDBvNjl4eGViIn0.oYDl6csdVuVzlB0hf2ju2Q';
 
-      // Add MapBox base layer: Satellite.
-      var opts = {
-        title: 'MapBox Satellite',
-        url: 'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=' + key,
-        group: 'Base layers',
-        base: true,
-        visible: false,
-      };
-      instance.addLayer('xyz', opts);
-
       // Add MapBox base layer: Outdoors.
       var opts = {
         title: 'MapBox Outdoors',
@@ -23,6 +13,16 @@
         group: 'Base layers',
         base: true,
         visible: false,
+      };
+      instance.addLayer('xyz', opts);
+
+      // Add MapBox base layer: Satellite.
+      var opts = {
+        title: 'MapBox Satellite',
+        url: 'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=' + key,
+        group: 'Base layers',
+        base: true,
+        visible: true,
       };
       instance.addLayer('xyz', opts);
 
